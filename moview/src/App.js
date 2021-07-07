1762b9722680d03b08c51e2cfbf35512586ca52f
Ghanshyam ram <Ghanshyam901@users.noreply.github.com>
2021-07-08 01:22:01 +0530

    routing

--- moview/src/App.js
+++ moview/src/App.js
@@ -1,9 +1,18 @@
 import Movies from './Components/Movies';
 // import './App.css';
+import Home from './Components/Home';
+import About from './Components/About';
+import {BrowserRouter,Route,Switch} from 'react-router-dom';
 
 function App() {
   return (
-  <Movies/>
+ <BrowserRouter>
+ <Switch>
+   <Route exact path ='/' component={Home}/>
+   <Route path ='/movies' component={Movies}/>
+   <Route path ='/about' component={About}/>
+ </Switch>
+ </BrowserRouter>
 
   );
 }
---
+++ moview/src/Components/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function About() {
+    return (
+        <div>
+            <h1>about section</h1>
+        </div>
+    )
+}
+
+export default About
